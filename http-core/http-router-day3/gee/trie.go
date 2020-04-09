b7b12f1f1020516a2f0c5eb1cc9276f91024d88e
zhanghuan <aystzh@163.com>
2020-04-09 22:13:16 +0800

    go router学习

--- http-core/http-router-day3/gee/trie.go
+++ http-core/http-router-day3/gee/trie.go
@@ -6,3 +6,23 @@ type node struct {
 	children []*node //子节点 例如：[doc, tutorial, intro]
 	isWild   bool    //是否为精确匹配 part 含有 : 或 * 时为true
 }
+
+//第一个匹配成功的节点 用于插入
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+//匹配所有成功的节点 用于查找
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+}
